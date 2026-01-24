994c52e9031c67be154705088843f75499de5d89
ShivangChaurasia <shiva17ng@gmail.com>
2026-01-24 13:15:33 +0530

    wrote few notes and tried making few inbuilt functions of array

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -28,12 +28,245 @@
     // console.log(area(5,4))
 
 // find a num ODD OR EVEN
-    function evenorodd(num){
-        if(num%2==0){
-            return "even";
-        }else{
-            return "odd"
-        }
-    }
+    // function evenorodd(num){
+    //     if(num%2==0){
+    //         return "even";
+    //     }else{
+    //         return "odd"
+    //     }
+    // }
 
-    console.log(evenorodd(6))
+    // console.log(evenorodd(6))
+
+    
+    
+    // create function to take 1 para num arr and it should add all values.
+    
+    // let arr = [1,2,3,4,5,6];
+    // function sum(arr){
+    //     let sum = 0;
+    //     for(num of arr){
+        //         sum+=num;
+        //     }
+        //     return sum;
+        // }
+        
+        // console.log(sum(arr));
+
+        
+        // wap takes  a var and returns it's types
+        
+        
+        // function detectType(value) {
+            //     let arr = [];
+            //     for(let i=0;i<value.length;i++){
+                //         arr.push(typeof(value[i]))
+                //     }
+                // return arr;
+                // }
+                // console.log(detectType([2, "rtyui", null, undefined, true]))
+                
+                // to check if the input is object or not
+                
+                // function check(input){
+    //     if(typeof(input)==="object"){
+        //         return true;
+        //     }else{
+            //         return false
+            //     }
+            // }
+            
+    // console.log(check([]))
+
+
+
+
+
+// Wap to concatenate the full name
+    // function Greeting(fname,lname, year){
+    //     console.log(`Hello, ${fname} ${lname} !\nI am in ${year}rd of B.Tech(CSE)`)
+    // }
+    // Greeting("shivang", "Chaurasia", 3)
+
+
+// Notes:-
+// Array in-built functions (Example: length, indexOf, push, pop, shift, unshift, slice, splice, concat, join, reverse, sort, map, filter, reduce, forEach, every, some)------------
+// let arr = [1,2,3,4,5,6];
+
+
+    // The in-built function of array
+        // console.log(arr)
+        // console.log(arr.length)
+        // console.log(arr[0])
+        // console.log(arr[1])
+        // console.log(arr[2])
+        // console.log(arr[3])
+        // console.log(arr[4])
+        // console.log(arr[5])
+    // ================================
+    // Length in-built funtion:
+        // console.log(arr.length)
+
+    // ==================================
+
+    // indexOf funtion:
+        // console.log(arr.indexOf(3))
+        // console.log(arr.indexOf(7))
+
+    // ==================================
+     // lastIndexOf funtion:
+        // console.log(arr.lastIndexOf(3))
+        // console.log(arr.lastIndexOf(7))
+
+    // ==================================
+    // slice funtion:
+        // console.log(arr.slice(2,4))
+        // console.log(arr.slice(2))
+
+    // ==================================
+    // splice funtion:
+        // console.log(arr.splice(2,4))
+        // console.log(arr.splice(2))
+
+    // ==================================
+    // concat funtion:
+        // console.log(arr.concat([7,8,9]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
