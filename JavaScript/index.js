8e3222c2a1b8dac36789239a3f49ce33a96f2a50
ShivangChaurasia <shiva17ng@gmail.com>
2026-01-24 10:56:31 +0530

    Got few basic knowledge of datatypes, history of Js, and tried few very basic function questions.

---
+++ JavaScript/index.js
@@ -0,0 +1,39 @@
+    console.log("Hello, We're in Console")
+    // Data Types
+        // var first;
+        // var first="first";
+        // first = "second"
+        // var first=1;
+        // console.log(first)
+
+        // let second;
+        // second = 2;
+        // second = 3;
+        // console.log(second)
+
+        // const third="third";
+        // console.log(third)
+
+    //Array
+        // const arr = [1,2,3,4];
+        // console.log(typeof(arr));
+
+
+
+// waf to cal. rectangle area
+    // function area(length, breadth){
+    //     return length*breadth;
+    // }
+
+    // console.log(area(5,4))
+
+// find a num ODD OR EVEN
+    function evenorodd(num){
+        if(num%2==0){
+            return "even";
+        }else{
+            return "odd"
+        }
+    }
+
+    console.log(evenorodd(6))
