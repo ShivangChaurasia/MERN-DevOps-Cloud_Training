af4022a91a604f14cea3d2e8634e9fb3d6651c79
ShivangChaurasia <shiva17ng@gmail.com>
2026-01-27 16:50:13 +0530

    Wrap up for the day

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -129,6 +129,13 @@ let arr = [1,2,3,4,5,6,7,8,9];
         // console.log(arr.splice(2,4))
         // console.log(arr)
 
+
+// let arr = [1,2,3,4,5,6];
+// arr.splice(4, 2, 0, 0)
+// console.log(arr) //will give [1, 2, 3, 4, 0, 0]
+
+
+
     // ==================================
     // concat funtion:
         // console.log(arr.concat([7,8,9]))
