556ec1b8c53f5870dacccc44d49fd3783ef52aa0
ShivangChaurasia <shiva17ng@gmail.com>
2026-01-30 15:47:38 +0530

    Completed Scope

---
+++ JavaScript/scope.js
@@ -0,0 +1,32 @@
+
+// let a=20
+// function value(){
+//     a=10
+//     console.log("internal",a);
+// }
+// value()
+// console.log("global: ",a) // Gets Updated
+
+
+// for(let i=0;i<10;i++){
+//     console.log(i)
+// }
+// console.log(i) // Will give error as it is not defined globally
+
+
+// console.log(a)
+
+// function print(){
+//     var a=20;
+//     console.log(a)
+// }
+// print()
+// console.log(a)
+
+
+
+for(var i=0;i<5;i++){
+    setTimeout(()=>{
+        console.log(i)
+    },10)
+}
