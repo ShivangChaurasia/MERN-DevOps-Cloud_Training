634f5c612ea674931fae13e8416d35e8395e2af4
ShivangChaurasia <shiva17ng@gmail.com>
2026-02-02 10:23:24 +0530

    Synchronous &  Asynchronous

---
+++ JavaScript/sync&async.js
@@ -0,0 +1,24 @@
+
+// Synchronous---------------------------------------
+// console.log("Process  Start");
+// for(let i=0;i<1000000000;i++){
+    //     if(i===899999999){
+        //         console.log("About to end");
+        //     }
+        // }
+        
+        // console.log("Process End")
+
+
+
+
+// Asynchronous---------------------------------------
+// console.log("Start");
+// let i=0;
+// setTimeout(() => {
+//     console.log(i++) //Not a loop
+//     console.log("Process"); 
+// }, 1000); //Working Upon timer
+
+// console.log("End");
+
