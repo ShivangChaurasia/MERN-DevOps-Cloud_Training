7713556f4245b0d9847f774b76bfcf3ea05c55fb
ShivangChaurasia <shiva17ng@gmail.com>
2026-02-10 10:52:32 +0530

    Had a practical visuals of Function mounting and unmounting

--- Reactjs/vite-Week2/src/App.jsx
+++ Reactjs/vite-Week2/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import '/src/App.css';
+import {useEffect, useState} from 'react';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // const [name,setName] = useState("Hello");
+  // console.log("Rendering Outer");
+  
+  let handleCount = ()=>{
+    setCount(count+1);
+  }
+  useEffect(()=>{
+    // console.log('Mounted or Updated')
+    // console.log('Data Modified')
+    document.title = `title Changed ${count}`;
+  },[count])
 
+  // let handleChange = ()=>{
+  //   setName("This got modified");
+  // }
+  
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>This is Counter using Vite App</h1>
+      <h2>Count: {count}</h2>
+      {/* <h2>{name}</h2> */}
+      <button onClick={handleCount}>Increase Count</button>
+      {/* <button onClick={handleChange}>Set Name</button> */}
     </>
   )
 }
