aff012fd67c9871f3807d6e777b74e5a8d473dd4
ShivangChaurasia <shiva17ng@gmail.com>
2026-02-13 13:28:47 +0530

    Updated Notes of Context API and Routing in React

--- Reactjs/vite-week2D3/src/component/Nav.jsx
+++ Reactjs/vite-week2D3/src/component/Nav.jsx
@@ -5,6 +5,12 @@ export default function Nav() {
     <>
         <h1>This is Nav</h1>
         <div>
+            {/* If we use Anchor tag, before going to every other page it gets refreshed. */}
+            {/* <a href='/'>Home</a> {" | "}
+            <a href='/about'>About</a>{" | "} 
+            <a href='/contact'>Contact</a> */}
+
+            {/* Where as Link Tag helps us to travel across pages without refreshing */}
             <Link to='/'>Home</Link> {" | "}
             <Link to='/about'>About</Link>{" | "}
             <Link to='/contact'>Contact</Link>
