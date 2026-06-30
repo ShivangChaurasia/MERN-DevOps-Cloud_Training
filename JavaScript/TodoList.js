949d90bc89e86df93fcb43c9ce23d4e065b0971c
ShivangChaurasia <shiva17ng@gmail.com>
2026-06-30 15:00:02 +0530

    Add input field for description and enhance Todo List functionality

--- JavaScript/TodoList.js
+++ JavaScript/TodoList.js
@@ -1,7 +1,10 @@
 let container = document.getElementById("container")
 
+
+
 function addItem(){
     let input = document.getElementById('input')
+    let Descinput = document.getElementById('input2')
 
 
     if (input.value.trim() === "") return;
@@ -17,8 +20,14 @@ function addItem(){
     textSpan.className = "text-gray-700 font-medium break-all pr-4";
     li.append(textSpan);
 
+
+    let DesctextSpan = document.createElement('span');
+    DesctextSpan.innerText = Descinput.value;
+    DesctextSpan.className = "text-gray-500 font-small break-all pr-4";
+    li.append(DesctextSpan);
+
     let button = document.createElement('button')
-    button.innerText = "✕" 
+    button.innerText = "✕"
 
 
     button.className = "flex items-center justify-center w-8 h-8 rounded-lg text-red-500 hover:text-white hover:bg-red-500 active:scale-95 transition-all duration-150 cursor-pointer text-sm font-bold shrink-0";
@@ -32,5 +41,7 @@ function addItem(){
 
 
     input.value = ""
+    Descinput.value = ""
     input.focus();
 }
+
