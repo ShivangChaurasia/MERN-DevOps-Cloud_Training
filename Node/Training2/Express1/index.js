fdb522906444e52f163376d8205fb1900680b809
ShivangChaurasia <shiva17ng@gmail.com>
2026-07-15 16:43:13 +0530

    Add initial Express setup and basic HTTP server implementation
    - Created package.json for Express application with necessary scripts and dependencies.
    - Added a simple HTTP server in http.js that responds with a greeting message.

---
+++ Node/Training2/Express1/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("This is get Request!");
+});
+
+app.listen(5000,()=>{
+    console.log("Server Running at post 5k...successfully!!")
+});
